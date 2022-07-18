ce7b4b19f861eb9cfb66032ba86b090634f0be5c
Vo Anh Hieu <voanhhieu10250@gmail.com>
2022-07-18 22:06:07 +0700

    add stock info block in my_portfolio route

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,11 +1,16 @@
-class Stock < ApplicationRecord
+# frozen_string_literal: true
 
+class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(
-      publishable_token: Rails.application.credentials.iex_client[:publishable_token],
-      secret_token: Rails.application.credentials.iex_client[:secret_token],
-      endpoint: 'https://sandbox.iexapis.com/v1'
-    )
-    client.price(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:publishable_token],
+                                  secret_token: Rails.application.credentials.iex_client[:secret_token],
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+    begin
+      new(ticker: ticker_symbol,
+          name: client.company(ticker_symbol).company_name,
+          last_price: client.price(ticker_symbol))
+    rescue StandardError => _e
+      nil
+    end
   end
 end
