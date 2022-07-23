fa3b54dc43b139a87bc1f694ce84930e56f75ee4
Vo Anh Hieu <voanhhieu10250@gmail.com>
2022-07-23 15:45:48 +0700

    Setup UserStock resource

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:publishable_token],
                                   secret_token: Rails.application.credentials.iex_client[:secret_token],
